d9546ad607e17f74c154be7d1b3b3f42990434d3
Cherkesky <guy@cherkesky.com>
2020-05-29 17:41:07 -0500

    scaffolding the ExerciseController

---
+++ StudentExercisesAPI/StudentExercisesAPI/Controllers/ExerciseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+
+namespace StudentExercisesAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
